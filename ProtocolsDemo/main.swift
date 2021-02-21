38b8bc53213d34c3bf89b07ae4ad415cd1dde48b
FrancaTM <francatm@outlook.com>
2021-02-20 23:05:24 -0300

    just a demo for protocols

--- ProtocolsDemo/main.swift
+++ ProtocolsDemo/main.swift
@@ -1,11 +1,52 @@
-//
-//  main.swift
-//  ProtocolsDemo
-//
-//  Created by Tulio Marcos Franca on 20/02/21.
-//
+protocol CanFly {
+    func fly()
+}
 
-import Foundation
+class Bird {
+    var isFemale = true
+    
+    func layEgg() {
+        if isFemale {
+            print("bird makes new bird in shell")
+        }
+    }
+    
+//    func fly() {
+//        print("bird flaps wings lifts off into sky")
+//    }
+}
 
-print("Hello, World!")
+class Eagle: Bird, CanFly {
+    func fly() {
+        print("eagle flaps wings lifts off into sky")
+    }
+    
+    func soar() {
+        print("eagle glides in air currents")
+    }
+}
 
+class Penguin: Bird {
+    func swim() {
+        print("penguin paddles through water")
+    }
+}
+
+struct FlyingMuseum {
+    func flyingDemo(flyingObject: CanFly) {
+        flyingObject.fly()
+    }
+}
+
+struct Airplane: CanFly {
+    func fly() {
+        print("airplane lift off into air")
+    }
+}
+
+let myEagle = Eagle()
+let myPenguin = Penguin()
+let myPlane = Airplane()
+
+let museum = FlyingMuseum()
+museum.flyingDemo(flyingObject: myPlane)
